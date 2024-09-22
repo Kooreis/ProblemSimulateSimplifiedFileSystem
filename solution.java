06f5b54e331ec0dd26c1a1fcc23d65b8fed544c9
Kooreis <kooreis.official@gmail.com>
2024-09-22 14:01:21 -0500

    In this update, content 'hello' is added to a file 'd' in the directory '/a/b/c'.

--- solution.java
+++ solution.java
@@ -1,8 +1,7 @@
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         FileSystem fs = new FileSystem();
         fs.mkdir("/a/b/c");
+        fs.addContentToFile("/a/b/c/d", "hello");
     }
 }
