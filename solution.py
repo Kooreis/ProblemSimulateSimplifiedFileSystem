aca092f6902f2e7729bbe092ac3a502f7ead5ee8
Kooreis <kooreis.official@gmail.com>
2024-09-22 14:01:21 -0500

    This is the 'ls' method. It lists all files and directories in a given directory. If the path is a file, it returns the file name. If the path is a directory, it returns a sorted list of its contents.

--- solution.py
+++ solution.py
@@ -1,3 +1,11 @@
-class FileSystem:
-    def __init__(self):
-        self.filepath = {}
+def ls(self, path: str):
+        if path in self.filepath:
+            if isinstance(self.filepath[path], str):
+                return [path.split('/')[-1]]
+            return sorted(self.filepath[path].keys())
+        else:
+            path = path.split('/')
+            node = self.filepath
+            for part in path[1:]:
+                node = node[part]
+            return sorted(node.keys())
